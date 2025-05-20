4e0ae29d5a82ef22c3d5513964cc1b3c7bc1d268
RedXST <leon@thea-markus.de>
2025-05-20 19:48:49 +0200

    Update SupermarktStarter.java

--- src/SupermarktStarter.java
+++ src/SupermarktStarter.java
@@ -2,12 +2,7 @@ public class SupermarktStarter {
 
 	public static void main(String[] args) {
 			
-		Datenbank datenbank = new Datenbank();
-		Beliebtheitsgraph beliebtheitsgraph = new Beliebtheitsgraph(datenbank);
-	    new Kassierer(datenbank, beliebtheitsgraph);
-	    new LagermitarbeiterView(datenbank, beliebtheitsgraph);
-	    new KundeView(datenbank);
-	    new ProduktverwaltungsView(datenbank);
+		new Dashboard();
 	        
 	}
 
