92774fbcce474541b4dd876494a352f4dad1e770
MelodicTrumpet <8ccpandacc8@gmail.com>
2025-05-23 12:13:45 +0200

    Dokumentation Kassierer

--- src/Kassierer.java
+++ src/Kassierer.java
@@ -5,12 +5,22 @@ public class Kassierer extends Controller {
 	private Beliebtheitsgraph beliebtheitsgraph;
 	private LinkedList<WarenkorbEintrag> warenkorb;
 
+	/**
+	 * erstellt einen neuen Kassierer
+	 * @param datenbank, auf die der Kassierer zugreifen kann
+	 * @param beliebtheitsgraph, in den er die Warenkörbe schickt, wo sie dann gespeichert werden
+	 */
 	public Kassierer(Datenbank datenbank, Beliebtheitsgraph beliebtheitsgraph) {
 		this.datenbank = datenbank;
 		this.warenkorb = new LinkedList<>();
 		this.beliebtheitsgraph = beliebtheitsgraph;
 	}
 
+	/**
+	 * fügt das neue Produkt in den Warenkorb hinzu
+	 * @param name, des gescannten Produkts
+	 * @param anzahl, wie oft es gekauft wird
+	 */
 	public void kassieren(String name, int anzahl) {
 		Produkt p = datenbank.produktSuchen(name);
 		if (p != null) {
@@ -18,14 +28,9 @@ public class Kassierer extends Controller {
 		}
 	}
 
-//	public void warenkorbBeenden() {
-//		for (WarenkorbEintrag eintrag : warenkorb) {
-//			datenbank.produktanzahlVeraendernK(eintrag.anzahl, eintrag.name);
-//		}
-//		datenbank.produkteSpeichern(); // Speichern nach allen Änderungen
-//		warenkorb.clear();
-//	}
-
+	/**
+	 * beendet den Warenkorb und übergibt die Liste an den Beliebtheitsgraoh weiter
+	 */
 	public void warenkorbBeenden() {
 		for (WarenkorbEintrag eintrag : warenkorb) {
 			datenbank.produktanzahlVeraendernK(eintrag.anzahl, eintrag.name);
@@ -42,28 +47,38 @@ public class Kassierer extends Controller {
 		}
 		beliebtheitsgraph.warenkorbHinzufuegen(produktWarenkorb);
 
-//		// Beliebteste 3 Produkte neben dem ersten ausgeben (z.B. basierend auf erstem Produkt im Warenkorb)
-//		if (!produktWarenkorb.isEmpty()) {
-//			String name = produktWarenkorb.getFirst().getName();
-//			//System.out.println("Beliebte Produkte zu „" + name + "“:");
-//			beliebtheitsgraph.beliebtheitAusgeben(name, 3);
-//		}
-
 		warenkorb.clear();
 	}
 
+	/**
+	 * sucht das Produkt in der Datenbank
+	 * @param name des gesuchten Produkts
+	 * @return das gesuchte Produkt, wenn es existiert
+	 */
 	public Produkt produktSuchen(String name) {
 		return datenbank.produktSuchen(name);
 	}
 
+	/**
+	 * lässt sich die Produkte als LinkedList von der Datenbank ausgeben
+	 * @return diese LinkedList
+	 */
 	public LinkedList<Produkt> produkteAusgeben() {
 		return datenbank.produkteAusgeben();
 	}
 
+	/**
+	 * Klasse die den Warenkrobeintrag speichert
+	 */
 	private static class WarenkorbEintrag {
 		String name;
 		int anzahl;
 
+		/**
+		 * speichert den Warenkorbeintrag
+		 * @param name des Produkts
+		 * @param anzahl wie viele Produkte verkauft wurden
+		 */
 		public WarenkorbEintrag(String name, int anzahl) {
 			this.name = name;
 			this.anzahl = anzahl;
